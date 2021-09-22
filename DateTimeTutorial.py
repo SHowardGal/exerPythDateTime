318d5c7ca3fe60148a731a229228c0adadbb2c26
SHowardGal <u0250173@utah.edu>
2021-09-21 18:23:26 -0600

    commit

---
+++ DateTimeTutorial.py
@@ -0,0 +1,60 @@
+import datetime
+
+t = datetime.time(9, 30, 45, 1000000)
+print(t)
+print(t.hour)
+
+dt=datetime.datetime(2016, 7, 26, 12, 30, 45, 100000)
+print(dt)
+
+tdelta = datetime.timedelta(days=7)
+
+print(dt.year)
+
+print(dt + tdelta)
+
+tdelta = datetime.timedelta(hours=12)
+print(dt + tdelta)
+
+dt_today=datetime.datetime.today()
+dt_now=datetime.datetime.now()
+dt_utcnow=datetime.datetime.utcnow()
+print(dt_today)
+print(dt_now)
+print(dt_utcnow)
+
+import pytz
+
+dt = datetime.datetime(2016, 7, 27, 12, 30, 45, tzinfo=pytz.UTC)
+print(dt)
+
+dt_now=datetime.datetime.now(tz=pytz.UTC)
+print(dt_now)
+
+dt_utcnow=datetime.datetime.utcnow().replace(txinfo=pytz.UTC)
+print(dt_utcnow)
+
+dt_mtn = dt_utcnow.astimezone(pytz.timezone('US/Mountain'))
+print(dt_mtn)
+
+for tz in pytz.all_timezones:
+    print(tz)
+
+dt_mtn = datetime.datetime.now()
+print(dt_mtn)
+
+mtn_tz = pytz.timezone('US/Mountain')
+dt_mtn = mtn_tz.localize(dt_mtn)
+print(dt_mtn)
+
+dt_east = dt_mtn.astimezone(pytz.timezone(US/Eastern))
+
+dt_mtn = dt_utcnow.astimezone(pytz.timezone('US/Mountain'))
+
+print(dt_mtn.isoformat())
+print(dt_mtn.strftime('%B %d, %Y'))
+
+dt_str = 'July 26, 2016'
+
+dt = datetime.datetime.strptime(dt_str), '%B %d, %Y')
+print(dt)
