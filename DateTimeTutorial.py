5b85b03e4811c9e0ea96faa6ab25f68ee3cfd6f0
SHowardGal <u0250173@utah.edu>
2021-09-26 15:47:31 -0600

    commit

--- DateTimeTutorial.py
+++ DateTimeTutorial.py
@@ -56,5 +56,5 @@ print(dt_mtn.strftime('%B %d, %Y'))
 
 dt_str = 'July 26, 2016'
 
-dt = datetime.datetime.strptime(dt_str), '%B %d, %Y')
+dt = datetime.datetime.strptime(dt_str), '%B %d, %Y'
 print(dt)
